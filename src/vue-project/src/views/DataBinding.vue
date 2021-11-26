94c6abe7b6c83f0adf923dfa5cd914cafa787162
jaeyoun.choi <jaeyoun.choi@softcamp.co.kr>
2021-11-26 14:30:38 +0900

    cjy 211126 vue.js 파일추가

---
+++ src/vue-project/src/views/DataBinding.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Hello, gslnbwobnewobnerojgerlgjkeg.je;lbjelgjeaglfamjwlgjweglkwjgmoiwrjboiwrjbgorejgnoa{{title}}!</h1>
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'world'
+    };
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
