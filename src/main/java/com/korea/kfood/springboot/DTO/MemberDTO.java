28e859e5af8b0b566b7b7b95ad0bae33062b1bb3
XXVIGRUFV <dark9723@gmail.com>
2022-06-06 22:43:00 +0900

    cjy 220606 환경구성 1

--- src/main/java/com/korea/kfood/springboot/DTO/MemberDTO.java
+++ src/main/java/com/korea/kfood/springboot/DTO/MemberDTO.java
@@ -1,29 +1,20 @@
 package com.korea.kfood.springboot.DTO;
 
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class MemberDTO {
 
-    //회원 ID(E-Mail)
-    private String memberId;
-
-    //회원 비밀번호
-    private String memberPw;
-
-    //회원 이름
-    private String memberName;
-
-    //회원 우편번호
-    private String memberAdd1;
-
-    //회원 주소
-    private String memberAdd2;
-
-    //회원 전화번호
-    private String memberPhone;
-
-    //회원 등록일
-    private String regDate;
-
-    //회원 포인트
-    private String point;
+    private String memberId; //회원 ID(E-Mail)
+    private String memberPw; //회원 비밀번호
+    private String memberName; //회원 이름
+    private String memberAdd1; //회원 우편번호
+    private String memberAdd2; //회원 주소
+    private String memberPhone;  //회원 전화번호
+    private String regDate;  //회원 등록일
+    private String point; //회원 포인트
 
 }
--- src/main/java/com/korea/kfood/springboot/controller/MemberController.java
+++ src/main/java/com/korea/kfood/springboot/controller/MemberController.java
@@ -1,16 +1,50 @@
 package com.korea.kfood.springboot.controller;
 
+import com.korea.kfood.springboot.DTO.MemberDTO;
+import com.korea.kfood.springboot.Service.MemberService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.lang.reflect.Member;
 
 @Controller
 @RequestMapping("/member")
 public class MemberController {
 
-    @GetMapping("/login")
-    public String login() {
-        return "forward:/index.html";
+    private static final Logger logger = LoggerFactory.getLogger(MemberController.class);
+
+    @Autowired
+    private MemberService memberService;
+
+    //회원가입
+    @PostMapping("/register")
+    public String registerMemberToSignUp(MemberDTO member) throws Exception {
+
+        logger.info("회원가입 페이지 진입");
+
+        memberService.memberJoin(member);
+
+        logger.info("회원가입 성공");
+
+        return "redirect: /main";
+    }
+
+    //로그인 페이지
+    @PostMapping("/login")
+    public String login(MemberDTO member) throws Exception{
+
+
+        logger.info("로그인 페이지  진입");
+
+
+        logger.info("로그인 성공");
+
+        return "redirect:/main";
     }
 
     @GetMapping("/signup")
@@ -23,8 +57,5 @@ public class MemberController {
         return "forward:/index.html";
     }
 
-    @GetMapping("/register")
-    public String register() {
-        return "forward:/index.html";
-    }
+
 }
