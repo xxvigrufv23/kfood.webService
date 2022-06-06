7a592216c2a630b9cbc5ef91f8b8d60ebac4dfbe
XXVIGRUFV <dark9723@gmail.com>
2022-06-06 22:48:23 +0900

    cjy 220606 환경구성 2

--- src/main/java/com/korea/kfood/springboot/Service/MemberService.java
+++ src/main/java/com/korea/kfood/springboot/Service/MemberService.java
@@ -1,10 +1,10 @@
 package com.korea.kfood.springboot.Service;
 
-
 import com.korea.kfood.springboot.DTO.MemberDTO;
 
 public interface MemberService {
 
+    //회원가입
     public void memberJoin(MemberDTO member) throws Exception;
 
 }
